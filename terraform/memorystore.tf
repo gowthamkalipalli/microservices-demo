57dd466df0c4890ab73c3a1e59a38db005c9212a
Gowtham Kalipalli <83296469+gowthamkalipalli@users.noreply.github.com>
2026-06-09 16:48:42 +0530

    Update memorystore.tf

--- terraform/memorystore.tf
+++ terraform/memorystore.tf
@@ -33,13 +33,22 @@ resource "google_redis_instance" "redis-cart" {
 # Edit contents of Memorystore kustomization.yaml file to target new Memorystore (redis) instance
 resource "null_resource" "kustomization-update" {
   provisioner "local-exec" {
-    interpreter = ["bash", "-exc"]
-    command     = "sed -i \"s/REDIS_CONNECTION_STRING/${google_redis_instance.redis-cart[0].host}:${google_redis_instance.redis-cart[0].port}/g\" ../kustomize/components/memorystore/kustomization.yaml"
+    interpreter = ["PowerShell", "-Command"]
+
+    command = <<-EOT
+      $file = "../kustomize/components/memorystore/kustomization.yaml"
+      (Get-Content $file) `
+        -replace "REDIS_CONNECTION_STRING", "${google_redis_instance.redis-cart[0].host}:${google_redis_instance.redis-cart[0].port}" |
+        Set-Content $file
+    EOT
   }
 
-  # count specifies the number of instances to create;
-  # if var.memorystore is true then the resource is enabled
-  count          = var.memorystore ? 1 : 0
+  count = var.memorystore ? 1 : 0
+
+  depends_on = [
+    google_redis_instance.redis-cart
+  ]
+}
 
   depends_on = [
     resource.google_redis_instance.redis-cart
